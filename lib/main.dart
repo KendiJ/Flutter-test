ac4d38ee60ec3493b0d90ad8d822a47311f5c564
Maureen Josephine <m.akelo@carepay.com>
2020-11-08 13:55:49 +0300

    change background color

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Tests'),
     );
   }
 }
